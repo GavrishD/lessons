630e262a77528b7ca9693c905fc2bd102371cf73
Daria Gavrish <121836323+GavrishD@users.noreply.github.com>
2023-04-01 02:59:47 +0300

    Add files via upload

---
+++ lesson_18/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("click", function (e) {
+    const targetElement = e.target;
+
+    if (targetElement.closest(".header-icon")) {
+        document.documentElement.classList.toggle("menu-open");
+        e.preventDefault();
+    }
+});
